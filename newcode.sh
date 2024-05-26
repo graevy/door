c6f7ff5e7e46833392a8f780ccae188c2b4c8e71
avery <avry@pm.me>
2024-05-26 04:05:33 -0700

    load-bearing debug bash scripts

---
+++ newcode.sh
@@ -0,0 +1,1 @@
+curl -X POST http://127.0.0.1:8080/add_code -H "Content-Type: application/json" -d '{"duration_seconds": 3600}'
---
+++ usecode.sh
@@ -0,0 +1,1 @@
+curl -X POST http://127.0.0.1:8080/verify_code -H "Content-Type: application/json"
